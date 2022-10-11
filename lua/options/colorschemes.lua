9c836358e958441a931d89dc171987016d88da4a
Pierre-Luc Pinet <plpinet@gmail.com>
2022-10-11 00:48:13 -0400

    use tokyonight as the colorscheme

--- lua/options/colorschemes.lua
+++ lua/options/colorschemes.lua
@@ -6,19 +6,19 @@ vim.g.sonokai_better_performance = true
 -- Available values: "hard", "medium"(default), "soft"
 vim.g.gruvbox_material_background = "medium"
 vim.g.gruvbox_material_better_performance = true
-vim.g.gruvbox_material_transparent_background ="1"
+vim.g.gruvbox_material_transparent_background ="0"
 
 -- material
 --vim.g.material_style = "darker"
 
-vim.g.sonokai_transparent_background = "1"
+vim.g.sonokai_transparent_background = "0"
 
 ---- Example config in Lua
 vim.g.tokyonight_style = "night"
 vim.g.tokyonight_italic_functions = true
 vim.g.tokyonight_sidebars = { "qf", "vista_kind", "terminal", "packer" }
 vim.g.tokyonight_colors = { hint = "orange", error = "#ff0000" }
-vim.g.tokyonight_transparent = true
+vim.g.tokyonight_transparent = false
 
 
-vim.cmd[[colorscheme gruvbox-material]]
+vim.cmd[[colorscheme tokyonight]]
