fa19f909fd07c999f4650cc2369fbca99c78baf9
Pierre-Luc Pinet <plpinet@gmail.com>
2024-01-01 23:32:07 -0500

    neotree: use new method of configuring follow_current_file

--- lua/plugins/_nvim-neo-tree.lua
+++ lua/plugins/_nvim-neo-tree.lua
@@ -122,7 +122,9 @@ require("neo-tree").setup({
           --"thumbs.db"
         },
       },
-      follow_current_file = true, -- This will find and focus the file in the active buffer every
+      follow_current_file = {
+        enabled = true
+      },
       -- time the current file is changed while the tree is open.
       group_empty_dirs = false, -- when true, empty folders will be grouped together
       hijack_netrw_behavior = "open_default", -- netrw disabled, opening a directory opens neo-tree
@@ -146,7 +148,9 @@ require("neo-tree").setup({
       }
     },
     buffers = {
-      follow_current_file = true, -- This will find and focus the file in the active buffer every
+      follow_current_file = {
+        enabled = true
+      },
       -- time the current file is changed while the tree is open.
       group_empty_dirs = true, -- when true, empty folders will be grouped together
       show_unloaded = false,
