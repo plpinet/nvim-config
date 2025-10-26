b472429fa63f13502fef22c2988c81d84debc86a
Pierre-Luc Pinet <plpinet@gmail.com>
2025-10-26 15:10:04 -0400

    Remove mini-indentscope plugin

--- lua/plugins/mini-indentscope.lua
+++
@@ -1,9 +0,0 @@
-return {
-  'echasnovski/mini.indentscope',
-  config = function()
-    require('mini.indentscope').setup({
-      symbol = "│",
-      options = { try_as_border = true },
-    })
-  end
-}
