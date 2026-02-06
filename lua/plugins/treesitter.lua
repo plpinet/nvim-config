4a4c015969035d504bc0f2602873a67a6dc83e3b
Pierre-Luc Pinet <plpinet@gmail.com>
2026-02-06 08:23:58 -0500

    Fix treesitter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,22 +1,18 @@
 return {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
-    config = function () 
-      local configs = require("nvim-treesitter.configs")
-
-      configs.setup({
-          ensure_installed = { "bash", "lua", "rust", "cmake", "comment", "cpp", "css",
-                               "dockerfile", "go", "graphql", "html", "http", "java",
-                               "javascript", "json", "json5", "make", "markdown",
-                               "markdown_inline", "perl", "php", "python", "regex",
-                               "ruby", "vim", "vimdoc", "yaml", "typescript",
-                               "terraform", "hcl", "starlark"
-                             },
-          sync_install = false,
-          highlight = {
-            enable = true,
-            additional_vim_regex_highlighting = false,
-          },
-        })
-    end
+    opts = {
+        ensure_installed = { "bash", "lua", "rust", "cmake", "comment", "cpp", "css",
+                             "dockerfile", "go", "graphql", "html", "http", "java",
+                             "javascript", "json", "json5", "make", "markdown",
+                             "markdown_inline", "perl", "php", "python", "regex",
+                             "ruby", "vim", "vimdoc", "yaml", "typescript",
+                             "terraform", "hcl", "starlark"
+                           },
+        sync_install = false,
+        highlight = {
+          enable = true,
+          additional_vim_regex_highlighting = false,
+        },
+    },
 }
